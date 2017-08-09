d8ef3f6c67603c877860d846d1659fb0fd48fcdf
rmgift <giftr@oregonstate.edu>
2017-08-09 15:59:35 -0700

    Add files via upload

--- dbcon.js
+++ dbcon.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
-  host            : 'oniddb.cws.oregonstate.edu',
-  user            : 'giftr-db',
+  host            : 'classmysql.engr.oregonstate.edu',
+  user            : 'cs340_giftr',
   password        : '97NozdvDu3yAZKt4',
-  database        : 'giftr-db'
+  database        : 'cs340_giftr'
 });
 
 module.exports.pool = pool;
